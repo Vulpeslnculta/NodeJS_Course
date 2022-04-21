49b3358e4e55cdf37777da86c15e4a467fd7439c
Dominik.WantuchEXT <Dominik.WantuchEXT@delaval.com>
2022-04-21 17:14:00 +0200

    Just a start of Dynamic content stage

---
+++ Dynamic_content/routes/shop.js
@@ -0,0 +1,14 @@
+const path = require('path');
+const express = require('express');
+const rootDir = require('../util/path');
+
+const adminData = require('../routes/admin');
+
+const router = express.Router();
+
+router.get('/', (req, res, next) => {
+    console.log(adminData.products)
+    res.render('shop', { prods: adminData.products, docTitle: 'Shop', path: '/' });
+});
+
+module.exports = router;
