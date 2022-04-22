9e90b7492c5bd23ae5daa28bbb0f6b4c346b98fc
Dominik.WantuchEXT <Dominik.WantuchEXT@delaval.com>
2022-04-22 10:41:32 +0200

    Debug

--- App.assigement/app.js
+++ App.assigement/app.js
@@ -1,18 +1,17 @@
-const path = require("path");
 const express = require("express");
+const path = require("path");
 const bodyParser = require("body-parser");
-
 const mainRoutes = require("./routes/main");
+
 
 const app = express();
 
-app.set("view engine", "ejs");
 app.set("views", "views");
+app.set("view engine", "ejs");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use(mainRoutes);
-
 app.listen(3000);
