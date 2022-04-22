786bfedcd1d4c3502c3aade05f23152d508ec246
Dominik.WantuchEXT <Dominik.WantuchEXT@delaval.com>
2022-04-22 10:14:46 +0200

    First Functional web page

--- Dynamic_content/app.js
+++ Dynamic_content/app.js
@@ -14,7 +14,7 @@ app.use(shopRoutes);
 
 app.use('/', (req, res, next) => {
     res.status(404);
-    res.sendFile(path.join(__dirname, 'views', '404.html'));
+    res.render('404', { pageTitle: "Page Not Found", path: '/404' });
 });
 
 
